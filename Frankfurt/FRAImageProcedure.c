2dd44b51753688d3f8cc91645df202ac19a1260b
ElenesGu <elenesgu@gmail.com>
2016-03-03 20:31:38 +0900

    Fix typographical error: renaming PARRELL to PARALLEL.

--- Frankfurt/FRAImageProcedure.c
+++ Frankfurt/FRAImageProcedure.c
@@ -1,22 +1,22 @@
 #include "FRAImageProcedure.h"
-#define _CPU_ONLY
-//#define _CPU_PARRELL
+#define _GPU_PARALLEL
+//#define _CPU_PARALLEL
 
 //Header files should be located in exact ifdef area.
 #ifdef _CPU_ONLY
 
 #endif
 
-#ifdef _CPU_PARRELL
+#ifdef _CPU_PARALLEL
 #include <omp.h>
 #endif
 
-#ifdef _GPU_PARRELL
+#ifdef _GPU_PARALLEL
 
 #endif
 
 int FRAInvertImage(FRARawImage* input) {
-	//Invert image with no parrellization.
+	//Invert image with no parallelization.
 #ifdef _CPU_ONLY
 	int maxBitsNum = input->height * input->width * 3;
 	for (int x = 0; x < maxBitsNum; x++) {
@@ -25,7 +25,7 @@ int FRAInvertImage(FRARawImage* input) {
 #endif
 
 	//Yeongseon Na
-#ifdef _CPU_PARRELL
+#ifdef _CPU_PARALLEL
 	int maxBitsNum = input->height * input->width * 3;
 	#pragma omp parallel for
 	for( int x = 0; x < maxBitsNum; x++)
@@ -35,7 +35,7 @@ int FRAInvertImage(FRARawImage* input) {
 #endif
 
 	//Kangsan Kim
-#ifdef _GPU_PARRELL
+#ifdef _GPU_PARALLEL
 
 #endif
 }
